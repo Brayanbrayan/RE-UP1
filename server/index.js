11797734b2dd72dd133947368b0244b6da3294d8
Brayan <bnjoroge259@gmail.com>
2024-06-07 14:56:01 +0300

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const app = express()
+const mysql = require("mysql")
+const cors = require("cors")
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "Admin123!",
+    database: "employeesystem"
+});
+
+app.post('/create', (req, res) => {
+    const name = req.body.name
+    const age = req.body.age
+    const country = req.body.country
+    const position = req.body.position
+    const wage = req.body.wage
+
+    db.query(
+        "INSERT INTO employees(name, age, country, position, wage) VALUES(?,?,?,?,?)",
+        [name,age,country,position,wage],
+        (err, result) => {
+            if(err){
+                console.log(err)
+        } else{
+            res.send("Employee added successfully")
+        }
+    }
+    );
+
+});
+
+app.listen(3001, () => {
+    console.log('Server started on port 3001');
+});
