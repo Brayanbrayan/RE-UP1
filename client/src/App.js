ca086663066ab64706656ae1bf99edfe77aeac2e
Brayan <bnjoroge259@gmail.com>
2024-06-12 09:01:57 +0300

    Fixed bug in index.js  updated styles in style.css and also enabled connectivity to database enabling post and get

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import './App.css';
-import axios from 'axios'
+import axios, { Axios } from 'axios'
 
 function App() {
   const [name, setName] = useState("");
@@ -10,17 +10,40 @@ function App() {
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
 
-const addEmployee = () => {
-  axios.post('http://localhost:3001/create', {
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const [newWage,setNewWage] = useState(0)
+
+  const addEmployee = () => {
+    axios.post('http://localhost:3001/create', {
     name: name,
-   age: age, 
-   country:country, 
-   position: position, 
-   wage:wage
-  }).then (()=> {
-    console.log("success")
-  })
-}
+    age: age, 
+     country:country, 
+    position: position, 
+    wage:wage
+    }).then (()=> {
+    setEmployeeList([...employeeList,
+       {name: name,
+         age: age, 
+         country: country,
+          position: position,
+          wage: wage
+        }])
+    })
+  }
+
+  const getEmployees = () => {
+    axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const updateEmployeeWage = (id) => {
+    axios.put("http://localhost:3001/update", {wage: newWage, id: id})
+    .then((response) => {
+      alert("update")
+    })
+  }
 
   return (
   <div className="App">
@@ -47,8 +70,32 @@ const addEmployee = () => {
      }}/>
      <button onClick={addEmployee}>Add Employee</button>
     </div>
+
+    <div className='employees'>
+      <button onClick={getEmployees}>Show Employees</button>
+
+      {employeeList.map((val, key)=>{
+        return <div className='employee'>
+          <div>
+          <h3>Name: {val.name}</h3>
+          <h3>Age: {val.age}</h3>
+          <h3>Country: {val.country}</h3>
+          <h3>Position: {val.position}</h3>
+          <h3>Wage: {val.wage}</h3>
+          </div>
+          <div>
+            <input type="text"
+             placeholder="2000.."
+             onChange={(event) => {
+              setNewWage(event.target.value);
+             }}
+             /> 
+            <button onClick={() => {updateEmployeeWage(val.id)}}>Update</button>
+           </div>
+        </div>
+      })}
+    </div>
   </div>
   );
 }
-
 export default App;
--- server/index.js
+++ server/index.js
@@ -34,6 +34,32 @@ app.post('/create', (req, res) => {
 
 });
 
+app.get("/employees", (req, res) => {
+    db.query("SELECT * FROM employees", (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
+  });
+
+  app.put("/update", (req, res) => {
+    const id = req.body.id
+    const wage = req.body.wage
+    db.query(
+        "UPDATE employees SET wage = ? WHERE id = ?",
+        [wage, id],
+        (err,result) => {
+            if(err){
+                console.log(err)
+            } else{
+                res.send("result")
+            }
+        }
+    )
+  })
+
 app.listen(3001, () => {
     console.log('Server started on port 3001');
 });
